42a76a6795589aeba401e9b3859491dcbc9ec879
Glory <gloryjos.jiovio@outlook.com>
2023-01-12 20:12:19 +0530

    updatetailwindcss,json,,calsulator.js file updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-
+import $ from jquery
+import '../calculator.js'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
